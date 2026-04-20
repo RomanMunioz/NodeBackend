07fdac0d1593c6d4134a5249e4e71710acf28898
Román Muñoz <romanmuos76@gmail.com>
2026-04-20 17:58:58 -0300

    fixing connection with db

--- db.js
+++ db.js
@@ -1,14 +1,6 @@
 // src/db.js
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const pool = mysql.createPool(process.env.MYSQL_ADDON_URI);
 export default pool;
 
