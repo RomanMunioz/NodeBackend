2f85563036f5b6ea72ad527b34f776d654916cf6
Román Muñoz <romanmuos76@gmail.com>
2026-04-20 21:05:12 -0300

    fixing connection with db2

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 // src/db.js
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool(process.env.MYSQL_ADDON_URI);
+const pool = mysql.createPool({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  port: process.env.MYSQL_ADDON_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 export default pool;
 
